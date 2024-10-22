2fcb69efcc9f6819b48ea28a9d18cf27d99dacee
jess <16634555+jwbla@users.noreply.github.com>
2024-10-22 21:10:38 +0000

    update env vars but also fix scripts to recursively copy

--- backup_conf.sh
+++ backup_conf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo Backing up config
-cp ~/.config/hypr/* ~/dev/dotfiles/hyprland
-cp ~/.config/waybar/* ~/dev/dotfiles/waybar
-cp ~/.config/kitty/* ~/dev/dotfiles/kitty
-cp ~/.config/alacritty/* ~/dev/dotfiles/alacritty
+cp -r ~/.config/hypr ~/dev/dotfiles/hyprland
+cp -r ~/.config/waybar ~/dev/dotfiles/waybar
+cp -r ~/.config/kitty ~/dev/dotfiles/kitty
+cp -r ~/.config/alacritty ~/dev/dotfiles/alacritty
 cp ~/.tmux.conf ~/dev/dotfiles/.tmux.conf
 cp ~/.zshrc ~/dev/dotfiles/.zshrc
 cp ~/.rgtv.env ~/dev/dotfiles/.rgtv.env
