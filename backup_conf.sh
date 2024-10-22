5646929cea361f995e2c79861bfa8e83be411e71
jess <16634555+jwbla@users.noreply.github.com>
2024-10-22 21:19:30 +0000

    add more recursive copies

--- backup_conf.sh
+++ backup_conf.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo Backing up config
-cp -r ~/.config/hypr ~/dev/dotfiles/hypr
-cp -r ~/.config/waybar ~/dev/dotfiles/waybar
-cp -r ~/.config/kitty ~/dev/dotfiles/kitty
+cp -r ~/.config/hypr ~/dev/dotfiles
+cp -r ~/.config/waybar ~/dev/dotfiles
+cp -r ~/.config/kitty ~/dev/dotfiles
 cp ~/.config/alacritty ~/dev/dotfiles/alacritty
 cp ~/.tmux.conf ~/dev/dotfiles/.tmux.conf
 cp ~/.zshrc ~/dev/dotfiles/.zshrc
