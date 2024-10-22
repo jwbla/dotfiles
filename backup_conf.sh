18c812a256b0a466e3206ca4a25c2e5fe81aa0e4
jess <16634555+jwbla@users.noreply.github.com>
2024-10-22 21:23:03 +0000

    update more stuff, almost done?

--- backup_conf.sh
+++ backup_conf.sh
@@ -4,7 +4,7 @@ echo Backing up config
 cp -r ~/.config/hypr ~/dev/dotfiles
 cp -r ~/.config/waybar ~/dev/dotfiles
 cp -r ~/.config/kitty ~/dev/dotfiles
-cp ~/.config/alacritty ~/dev/dotfiles/alacritty
+cp -r ~/.config/alacritty ~/dev/dotfiles
 cp ~/.tmux.conf ~/dev/dotfiles/.tmux.conf
 cp ~/.zshrc ~/dev/dotfiles/.zshrc
 cp ~/.rgtv.env ~/dev/dotfiles/.rgtv.env
--- install_config.sh
+++ install_config.sh
@@ -7,7 +7,7 @@ echo Installing config
 cp -r ~/dev/dotfiles/hypr ~/.config
 cp -r ~/dev/dotfiles/waybar ~/.config
 cp -r ~/dev/dotfiles/kitty ~/.config
-cp ~/dev/dotfiles/alacritty ~/.config/alacritty
+cp -r ~/dev/dotfiles/alacritty ~/.config
 cp ~/dev/dotfiles/.tmux.conf ~/.tmux.conf 
 cp ~/dev/dotfiles/.zshrc ~/.zshrc
 cp ~/dev/dotfiles/.rgtv.env ~/.rgtv.env
